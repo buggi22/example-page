f7644061725453c8a876a3f6b86f2c491a142059
Andrew Leverentz <aleverentz@noreply>
2021-07-04 22:08:08 -0700

    First draft of router app

---
+++ src/About.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function About(props) {
+  return <div>This is what the About section looks like</div>;
+}
+
+export default About;
---
+++ src/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home(props) {
+  return <div>This is what the Home section looks like</div>;
+}
+
+export default Home;
